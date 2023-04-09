6a32ff0684ac0ed8d463dd71b20cd728dae3188d
Calista Mayer <calistammayer@gmail.com>
2023-04-08 21:29:45 -0400

    wip

--- BET.py
+++ BET.py
@@ -5,10 +5,23 @@ class BETNode:
 
     # Some class variables (no need to make a copy of these for every node)
     # access these with e.g. `BETNode.OPERATORS`
-    OPERATORS = {'+', '-', '*', '/'}
-    CARD_VAL_DICT = {'A':1, '1':1, '2':2, '3':3, '4':4,
-                     '5':5, '6':6, '7':7, '8':8, '9':9,
-                     '10':10, 'J':11, 'Q':12, 'K':13}
+    OPERATORS = {"+", "-", "*", "/"}
+    CARD_VAL_DICT = {
+        "A": 1,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "10": 10,
+        "J": 11,
+        "Q": 12,
+        "K": 13,
+    }
 
     def __init__(self, value, left=None, right=None):
         self.value = value
@@ -19,23 +32,72 @@ class BETNode:
     # and compare them (so you can write unittests more easily).
     def __eq__(self, other):
         """Two nodes are equal if their values are equal and their subtrees are (recursively) equal"""
-        if other is None: return False
-        return self.value == other.value and self.left == other.left and self.right == other.right
-    
+        if other is None:
+            return False
+        return (
+            self.value == other.value
+            and self.left == other.left
+            and self.right == other.right
+        )
+
     def __hash__(self):
         """Hash the whole tree (value + left and right subtrees)"""
         return hash((self.value, self.left, self.right))
-    
+
     # START HERE
-    def add_left(): pass
+    def add_left(self, left):
+        self.left = left
 
-    def add_right(): pass
+    def add_right(self, right):
+        self.right = right
 
-    def evaluate(): pass
-    
-    def __repr__(): pass
+    def evaluate(self, postfix):
+        """Evaluate the node and return the result"""
+        if self.value in BETNode.OPERATORS:
+            return self.value
+        if self.value == "+":
+            return self.left.evaluate(postfix) + self.right.evaluate(postfix)
+        if self.value == "-":
+            return self.left.evaluate(postfix) - self.right.evaluate(postfix)
+        if self.value == "*":
+            return self.left.evaluate(postfix) * self.right.evaluate(postfix)
+        if self.value == "/":
+            return self.left.evaluate(postfix) / self.right.evaluate(postfix)
+
+        else:
+            return self.CARD_VAL_DICT[self.value]
+
+        # if self.value == "=":
+        #     return self.left.evaluate(postfix) == self.right.evaluate(postfix)
+
+        # for item in postfix:
+        #     if isinstance(self.CARD_VAL_DICT[item], int):
+        #         pass
+        #     if self.CARD_VAL_DICT[item] == "=":
+        #         pass
+        #     if self.CARD_VAL_DICT[item] == "+":
+        #         pass
+        #     if self.CARD_VAL_DICT[item] == "-":
+        #         pass
+        #     if self.CARD_VAL_DICT[item] == "*":
+        #         pass
+        #     if self.CARD_VAL_DICT[item] == "/":
+        #         pass
+
+    def postfix_to_infix(self, postfix):
+        """Convert a postfix expression to an infix expression"""
+        stack = []
+        for item in postfix:
+            # if integer, keep going, once operator: take previous two items and pop them out
+            pass
+
+    def __repr__(self):
+        return f"{self.value} {self.left} {self.right}"
 
 
-def create_trees(): pass
+def create_trees():
+    pass
 
-def find_solutions(): pass
+
+def find_solutions():
+    pass
